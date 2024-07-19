8953c5449fffa416b6b1824ff372b59b02b5325c
yangwenjie1231 <138745068+yangwenjie1231@users.noreply.github.com>
2024-07-19 15:10:46 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from maix import camera, display, image, nn, app
+
+classifier = nn.Classifier(model="model_139629.mud")
+
+cam = camera.Camera(classifier.input_width(), classifier.input_height(), classifier.input_format())
+disp = display.Display()
+
+while not app.need_exit():
+    img = cam.read()
+    res = classifier.classify(img)
+    max_idx, max_prob = res[0]
+    msg = f"{max_prob:5.2f}: {classifier.labels[max_idx]}"
+    img.draw_string(10, 10, msg, image.COLOR_RED)
+    img = img.resize(disp.width(), disp.height(), image.Fit.FIT_CONTAIN)
+    disp.show(img)
---
+++ model_139629.mud
@@ -0,0 +1,12 @@
+
+[basic]
+type = cvimodel
+model = model_139629.cvimodel
+
+[extra]
+model_type = classifier
+input_type = rgb
+mean = 123.5, 123.5, 123.5
+scale = 0.017124753831663668, 0.017124753831663668, 0.017124753831663668
+labels = jinyinhua, baihe, dangshen, huaihua, gouqi
+
